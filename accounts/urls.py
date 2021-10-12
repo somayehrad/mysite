fc5244d8d4836fbbe11870e7ccef0b9f1b2c0e94
somayehrad <somayeh.navabirad@gmail.com>
2021-10-12 14:02:54 +0330

    phase 1

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from .import views
+
+app_name = 'accounts'
+urlpatterns = [
+    path('login/',views.user_login, name='login'),
+    path('logout/',views.user_logout, name='logout'),
+    path('register/',views.user_register, name='register'),
+    
+
+]
